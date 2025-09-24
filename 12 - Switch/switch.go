75eddbfe948e6fead2545a01e1fe1afaf76b9fe7
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2025-09-23 23:57:48 -0300

    Switch em Go

---
+++ 12 - Switch/switch.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+func diaDaSemana(numero int) string {
+	switch numero {
+	case 1:
+		return "Domingo"
+	case 2:
+		return "Segunda-feira"
+	case 3:
+		return "Terça-feira"
+	case 4:
+		return "Quarta-feira"
+	case 5:
+		return "Quinta-feira"
+	case 6:
+		return "Sexta-feira"
+	case 7:
+		return "Domingo"
+	default:
+		return "Número inválido"
+	}
+}
+
+func diaDaSemana2(numero int) string {
+	var diaDaSemana string
+
+	switch {
+	case numero == 1:
+		diaDaSemana = "Domingo"
+		fallthrough
+	case numero == 2:
+		diaDaSemana = "Segunda-Feira"
+	case numero == 3:
+		diaDaSemana = "Terça-Feira"
+	case numero == 4:
+		diaDaSemana = "Quarta-Feira"
+	case numero == 5:
+		diaDaSemana = "Quinta-Feira"
+	case numero == 6:
+		diaDaSemana = "Sexta-Feira"
+	case numero == 7:
+		diaDaSemana = "Sábado"
+	default:
+		diaDaSemana = "Número Inválido"
+	}
+
+	return diaDaSemana
+}
+
+func main() {
+	dia := diaDaSemana(200)
+	fmt.Println(dia)
+
+	fmt.Println("---------------")
+	dia2 := diaDaSemana2(10)
+	fmt.Println(dia2)
+}
