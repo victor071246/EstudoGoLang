c07b4b83b19a1f27a31178c3c07c13ac6921ac4c
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2025-10-05 19:04:27 -0300

    Funções avançadas em GO

---
+++ 14 - Funções Avançadas/14.3 - Funções Anônimas/anonimas.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	retorno := func(texto string) string{
+		return fmt.Sprintf("Recebido -> %s", texto)
+	}("Passando Parâmetro")
+
+	fmt.Printf(retorno)
+
+}
