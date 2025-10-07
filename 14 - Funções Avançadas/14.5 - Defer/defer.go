be781c06cc12d2bd27ab6938c9d0b4c855465603
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2025-10-06 23:55:49 -0300

    Funções com defer

---
+++ 14 - Funções Avançadas/14.5 - Defer/defer.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func funcao1() {
+	fmt.Println("Executando a função um")
+}
+
+
+func funcao2() {
+	fmt.Println("Executando a função dois")
+}
+
+func alunoEstaAprovado(n1, n2 float32) bool {
+	defer fmt.Println("Média calculada. Resultado será retornado!")
+	fmt.Println("Entrando na função para verificar se o aluno está aprovado")
+
+	media := (n1 + n2) / 2
+
+	if media >= 6 {
+		return  true
+	}
+
+	return false
+}
+
+func main() {
+	fmt.Println(alunoEstaAprovado(7, 8))
+}
