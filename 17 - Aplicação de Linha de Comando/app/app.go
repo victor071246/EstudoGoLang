ad387fb89068faf16e95d4aab2e76c7a74d9a87e
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2025-10-11 00:23:27 -0300

    Finalizando a aplicação de resolução de domínio para ips e servidores de domínio

--- 17 - Aplicação de Linha de Comando/app/app.go
+++ 17 - Aplicação de Linha de Comando/app/app.go
@@ -13,18 +13,27 @@ func Gerar() *cli.App{
 	app := cli.NewApp()
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca IPs e Nomes de Servidor na internet"
+	
+	flags := []cli.Flag {
+				cli.StringFlag{
+					Name: "host",
+					Value: "devbook.com.br",
+				},
+			}
 
 	app.Commands = []cli.Command{
 		{
 			Name: "ip",
 			Usage: "Busca IPS de endereços na internet",
-			Flags: []cli.Flag {
-				cli.StringFlag{
-					Name: "host",
-					Value: "devbook.com.br",
-				},
-			},
+			Flags: flags,
 			Action: buscarIps,
+		},
+		{
+			Name: "servidores",
+			Usage: "Busca o nome dos servidores na internet",
+			Flags: flags,
+			Action: buscarServidores,
+
 		},
 	}
 
@@ -41,5 +50,18 @@ func buscarIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
 	}
 }
