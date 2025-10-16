215fe2c914c51e875049feee6e1de7b1ed86b6a0
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2025-10-15 22:57:00 -0300

    HTML, CSS e JS em Go

---
+++ 22 - HTML, CSS E JS/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var templates *template.Template
+
+type usuario struct {
+	Nome string
+	Email string
+}
+
+func main() {
+
+	templates = template.Must(template.ParseGlob("*.html"))
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+
+		u := usuario{
+			"João",
+			"joao.pedro@gmail.com",
+		}
+
+		templates.ExecuteTemplate(w, "home.html", u)
+	})
+
+	fmt.Println("Escutando na porta 5000")
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
